1e53a409f2e0b03e3c30da84abb63e37827db99e
Dilantha Wijesinghe <wdilantha66@gmail.com>
2025-07-07 11:48:43 +0530

    feat: Cli functionality and styling.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,105 @@
+use chrono::{DateTime, Utc};
+use clap::Parser;
+use owo_colors::OwoColorize;
+use serde::Serialize;
+use std::path::Path;
+use std::{fs, path::PathBuf};
+use strum::Display;
+use tabled::settings::object::{Columns, Rows};
+use tabled::settings::{Color, Style};
+use tabled::{Table, Tabled};
+
+#[derive(Debug, Display, Serialize)]
+enum EntryType {
+    File,
+    Dir,
+}
+
+#[derive(Debug, Tabled, Serialize)]
+struct FileEntry {
+    #[tabled{rename="Name"}]
+    name: String,
+    #[tabled{rename="Type"}]
+    e_type: EntryType,
+    #[tabled{rename="Size B"}]
+    len_bytes: u64,
+    modified: String,
+}
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = "The Octo Waffle CLI")]
+struct Cli {
+    path: Option<PathBuf>,
+    #[arg(short, long)]
+    json: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let path = cli.path.unwrap_or(PathBuf::from("."));
+
+    if let Ok(does_exist) = fs::exists(&path) {
+        if does_exist {
+            if cli.json {
+                let get_files = get_files(&path);
+                println!(
+                    "{}",
+                    serde_json::to_string(&get_files).unwrap_or("Cannot parse to json".to_string())
+                );
+            } else {
+                print_table(path);
+            }
+        } else {
+            println!("{}", "Path does not exist".red());
+        }
+    } else {
+        println!("{}", "Error reading directory".red());
+    }
+}
+
+fn print_table(path: PathBuf) {
+    let get_files = get_files(&path);
+    let mut table = Table::new(get_files);
+    table.with(Style::rounded());
+    table.modify(Columns::first(), Color::FG_BRIGHT_CYAN);
+    table.modify(Columns::one(2), Color::FG_BRIGHT_MAGENTA);
+    table.modify(Columns::one(3), Color::FG_BRIGHT_YELLOW);
+    table.modify(Rows::first(), Color::FG_BRIGHT_GREEN);
+    println!("{}", table)
+}
+
+fn get_files(path: &Path) -> Vec<FileEntry> {
+    let mut data = Vec::default();
+    if let Ok(read_dir) = fs::read_dir(path) {
+        for entry in read_dir {
+            if let Ok(file) = entry {
+                map_data(file, &mut data);
+            }
+        }
+    }
+    data
+}
+
+fn map_data(file: fs::DirEntry, data: &mut Vec<FileEntry>) {
+    if let Ok(meta) = fs::metadata(&file.path()) {
+        data.push(FileEntry {
+            name: file
+                .file_name()
+                .into_string()
+                .unwrap_or("unknown name".into()),
+            e_type: if meta.is_dir() {
+                EntryType::Dir
+            } else {
+                EntryType::File
+            },
+            len_bytes: meta.len(),
+            modified: if let Ok(modi) = meta.modified() {
+                let date: DateTime<Utc> = modi.into();
+                format!("{}", date.format("%a %b %e %Y"))
+            } else {
+                String::default()
+            },
+        });
+    }
 }
